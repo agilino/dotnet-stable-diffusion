9408a41d12952f49eb30cb700a55daf525360556
ttlinh <totuanlinh@gmail.com>
2023-06-30 20:47:55 +0700

    feat: update config auto mapper

--- backend-api/Services/ImageService.cs
+++ backend-api/Services/ImageService.cs
@@ -1,31 +1,31 @@
-﻿using AutoMapper;
+﻿using System.Collections.Generic;
+using AutoMapper;
 using backend_api.Context;
 using backend_api.DTO;
 using backend_api.Entities;
 using backend_api.Mappers;
-using static StackExchange.Redis.Role;
 
 namespace backend_api.Services
 {
 	public class ImageService
 	{
 		readonly ApplicationDBContext _applicationDBContext;
-		readonly Mapper _mapper;
+		readonly IMapper _mapper;
 
-		public ImageService(ApplicationDBContext applicationDBContext, Mapper mapper) {
+		public ImageService(ApplicationDBContext applicationDBContext, IMapper mapper) {
 			_applicationDBContext = applicationDBContext;
 			_mapper = mapper;
 		}
 
 		public List<ImageResponseDTO> GetAllImages() {
 			var images = _applicationDBContext.Images.ToList();
-			var imageDTOS = MapperConfig.InitializeAutoMapper().Map<List<ImageResponseDTO>>(images);
-			return imageDTOS;
+            var imageDTOS = _mapper.Map<List<ImageResponseDTO>>(images);
+            return imageDTOS;
         }
 
         internal void createImage(ImageRequestDTO imageRequestDTO)
         {
-			var image = MapperConfig.InitializeAutoMapper().Map<Image>(imageRequestDTO);
+			var image = _mapper.Map<Image>(imageRequestDTO);
 			_applicationDBContext.Images.Add(image);
         }
 
