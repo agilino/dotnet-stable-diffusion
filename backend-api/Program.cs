2e8efe37b5f23e105619006a44b8ed7f717204a6
DESKTOP-I3LBHCS\ASUS <leahuy2912001.it@gmail.com>
2023-07-16 15:41:49 +0700

    feature: enhance C# Backend

--- backend-api/Program.cs
+++ backend-api/Program.cs
@@ -1,4 +1,5 @@
 ﻿using backend_api.Context;
+using backend_api.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IImageGenerationService, ImageGenerationService>();
 
 // Add Redis
 builder.Services.AddStackExchangeRedisCache(options =>
