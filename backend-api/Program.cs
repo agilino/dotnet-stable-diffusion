38b8378528be752d1baedf1883e79da9aa070115
lephamthanhdanh <lephamthanhdanh@gmail.com>
2023-07-27 13:34:57 +0700

    feat/excercise-format

--- backend-api/Program.cs
+++ backend-api/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Add dependency injection
 builder.Services.AddScoped<IImageGenerationService, ImageGenerationService>();
 
 
